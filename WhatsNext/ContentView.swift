eff72d6e6ac969b736c4aaaa756677702d7f5e00
Luca <luca@weve-got-it.com>
2022-08-21 19:33:28 +0200

    Initial Commit

---
+++ WhatsNext/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsNext
+//
+//  Created by Luca Capriati on 2022/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsNext/WhatsNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNextApp.swift
+//  WhatsNext
+//
+//  Created by Luca Capriati on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
