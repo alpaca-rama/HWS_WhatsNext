8b2ef2d26fcb43aa4fa9389c66f93c8cafb09599
Luca <luca@weve-got-it.com>
2022-08-21 21:17:35 +0200

    Completed Menu Bar application

--- WhatsNext/WhatsNextApp.swift
+++ WhatsNext/WhatsNextApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct WhatsNextApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
